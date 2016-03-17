82108bbcd6aebdc5f07c49b9757dd3f28b391389
koriley <kevin@blueshorestudios.com>
2016-03-17 15:51:45 -0500

    created the view dump for a single event.

--- eventCalendar/control/control_event.php
+++ eventCalendar/control/control_event.php
@@ -19,4 +19,40 @@ $id = $_GET['eventNum'];
 $res = $event->getSingleEvent($dbCon, $id);
 
 
-print_r($res);
+//print_r($res);
+
+echo '<div class="singleEvent">
+    <h1>' . $res[0]['title'] . '</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 location">
+                ' . $res[0]['location'] . '
+                <div class="address">
+                    ' . $res[0]['address'] . '
+                </div>';
+if ($res[0]['city'] != '') {
+    echo '<div class="cityState">' . $res[0]['city'] . ',' . $res[0]['state'] . '</div>';
+}
+if ($res[0]['phone'] != '') {
+    echo '<div class="phone">' . $res[0]['phone'] . '</div>';
+}
+if ($res[0]['url'] != '') {
+    echo '<div class="url"><a href="' . $res[0]['url'] . '">' . $res[0]['url'] . '</a></div>';
+}
+
+
+echo' </div>
+            <div class="col-md-4 time">' . $res[0]['starttime'] . ' - ' . $res[0]['endtime'] . '</div>
+                  
+        </div>
+        <div class="row-fluid">
+             <div class="desc">' . $res[0]['description'] . '</div>';
+if ($res[0]['image'] != '') {
+    echo '<div class="photo"><img src="http://417mag.com' . $res[0]['image'] . '" /></div>';
+}
+echo '</div>
+    </div>
+
+</div>';
+?>
+
